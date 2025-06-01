be2657cab3399a4136870d17692c87c826bd7f16
Lev Zupancic <levzupancic@gmail.com>
2025-06-01 15:41:25 +0200

    particles

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -8,6 +8,7 @@ class_name Hitscan_Weapon extends Node3D
 @export var recoil: float = 0.05
 @export var weapon_mesh: Node3D
 @export var weapon_damage: int = 15
+@export var muzzle_flash: GPUParticles3D
 
 func _ready() -> void:
 	pass
@@ -19,6 +20,7 @@ func _process(delta: float) -> void:
 	weapon_mesh.position = weapon_mesh.position.lerp(starting_weapon_position, delta * 10.0)
 
 func shoot() -> void:
+	muzzle_flash.restart()
 	cooldown_timer.start(1.0 / fire_rate)
 	var collider = ray_cast_3d.get_collider()
 	printt("Fired!", collider)
