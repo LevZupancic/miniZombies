5be4224b42a1e41facf4b28a978a9846e4241733
Lev Zupancic <levzupancic@gmail.com>
2025-06-05 22:03:48 +0200

    Refactor directory structure and add WeaponResource

---
+++ Entities/Weapons/WeaponResource.gd
@@ -0,0 +1,27 @@
+class_name Weapon_Resource extends Resource
+
+@export var name: String
+@export var weapon_type: WeaponType
+@export var fire_type: FireType
+
+@export var damage: int
+@export var firerate: float
+@export var recoil: float
+@export var mag_size: int
+@export var max_ammo: int
+@export var price: int
+
+enum FireType {
+	AUTOMATIC = 1,
+	SEMIAUTO = 2,
+	MELEE = 3
+}
+
+enum WeaponType {
+	PISTOL = 1,
+	SMG = 2,
+	RIFLE = 3,
+	LMG = 4,
+	SNIPER = 5,
+	KNIFE = 6
+}
